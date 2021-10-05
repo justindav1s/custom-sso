1dce40436d2c92a6e8aaa6f14b7112a1cc7c8317
Justin Davis <justinndavis@gmail.com>
2021-10-05 12:34:24 +0100

    fixes

--- build74/build.sh
+++ build74/build.sh
@@ -16,21 +16,20 @@ done
 
 oc  project $PROJECT
 
-oc delete all -l app=$APP
-
 oc create secret docker-registry quay-dockercfg \
   --docker-server=${QUAYIO_HOST} \
   --docker-username=${QUAYIO_USER} \
   --docker-password=${QUAYIO_PASSWORD} \
   --docker-email=${QUAYIO_EMAIL} \
-  -n ${PROD_PROJECT}
+  -n ${PROJECT}
 
-oc new-app -f docker-build-template.yaml \
+oc apply -f docker-build-template.yaml \
     -p APPLICATION_NAME=custom-sso \
     -p SOURCE_REPOSITORY_URL="https://github.com/justindav1s/custom-sso.git" \
     -p SOURCE_REPOSITORY_REF="master" \
     -p DOCKERFILE_PATH="build" \
-    -p DOCKERFILE_NAME="Dockerfile"
+    -p DOCKERFILE_NAME="Dockerfile" \
+    -n PROJECT
 
 oc start-build custom-sso-docker-build --follow
 
