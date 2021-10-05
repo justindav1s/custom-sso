dba1fb7d04bae8a4decb487a3c1d40c75a3a9296
Justin Davis <justinndavis@gmail.com>
2021-10-05 12:41:54 +0100

    74 update

--- build74/build.sh
+++ build74/build.sh
@@ -23,13 +23,13 @@ oc create secret docker-registry quay-dockercfg \
   --docker-email=${QUAYIO_EMAIL} \
   -n ${PROJECT}
 
-oc apply -f docker-build-template.yaml \
+oc process -f docker-build-template.yaml \
     -p APPLICATION_NAME=custom-sso \
     -p SOURCE_REPOSITORY_URL="https://github.com/justindav1s/custom-sso.git" \
     -p SOURCE_REPOSITORY_REF="master" \
     -p DOCKERFILE_PATH="build" \
     -p DOCKERFILE_NAME="Dockerfile" \
-    -n ${PROJECT}
+    -n ${PROJECT} | oc create -f -
 
 oc start-build custom-sso-docker-build --follow
 
