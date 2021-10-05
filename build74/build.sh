d35cd5972068f61682436e0603a5876fd0808f78
Justin Davis <justinndavis@gmail.com>
2021-10-05 12:45:53 +0100

    fix

--- build74/build.sh
+++ build74/build.sh
@@ -27,7 +27,7 @@ oc process -f docker-build-template.yaml \
     -p APPLICATION_NAME=custom-sso \
     -p SOURCE_REPOSITORY_URL="https://github.com/justindav1s/custom-sso.git" \
     -p SOURCE_REPOSITORY_REF="master" \
-    -p DOCKERFILE_PATH="build" \
+    -p DOCKERFILE_PATH="build74" \
     -p DOCKERFILE_NAME="Dockerfile" \
     -n ${PROJECT} | oc create -f -
 
