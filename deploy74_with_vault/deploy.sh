01f6bcc69d73562016adbc7db14bd09811fb84ad
Justin Davis <justinndavis@gmail.com>
2021-10-12 11:50:45 +0100

    updates

--- deploy74_with_vault/deploy.sh
+++ deploy74_with_vault/deploy.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
-oc login https://${IP} -u justin
+# login
 
-TLS_DOMAIN=openshiftlabs.net
-PROJECT=custom-sso
+PROJECT=redhat-sso
 
-# oc delete project $PROJECT
-# oc new-project $PROJECT 2> /dev/null
-# while [ $? \> 0 ]; do
-#     sleep 1
-#     printf "."
-# oc new-project $PROJECT 2> /dev/null
-# done
+oc project ${PROJECT}
+
+oc delete deployment sso
+oc delete route -l application=sso
+oc delete service -l application=sso
 
 oc policy add-role-to-user view system:serviceaccount:${PROJECT}:default
-
-oc create secret generic sso-jgroup-secret --from-file=certs/jgroups.jceks
-oc create secret generic sso-tls-secret --from-file=certs/${TLS_DOMAIN}.jks
-oc create secret generic sso-app-secret --from-file=certs/${TLS_DOMAIN}.jks
-oc secrets link default sso-jgroup-secret sso-tls-secret sso-app-secret
 
 oc create secret generic jboss-vault \
   --from-file=secrets/VAULT.dat \
@@ -26,21 +18,6 @@ oc create secret generic jboss-vault \
 
 oc new-app -f sso74-https.yaml \
  -p APPLICATION_NAME="sso" \
- -p HTTPS_SECRET="sso-tls-secret" \
- -p HTTPS_KEYSTORE="${TLS_DOMAIN}.jks" \
- -p HTTPS_KEYSTORE_TYPE="JKS" \
- -p HTTPS_NAME="sso" \
- -p HTTPS_PASSWORD="changeme" \
- -p DB_MIN_POOL_SIZE="3" \
- -p DB_MAX_POOL_SIZE="10" \
- -p JGROUPS_ENCRYPT_SECRET="sso-jgroup-secret" \
- -p JGROUPS_ENCRYPT_KEYSTORE="jgroups.jceks" \
- -p JGROUPS_ENCRYPT_NAME="jgroups" \
- -p JGROUPS_ENCRYPT_PASSWORD="changeme" \
- -p SSO_ADMIN_USERNAME="admin" \
- -p SSO_ADMIN_PASSWORD="admin" \
  -p SSO_REALM="demorealm" \
- -p SSO_TRUSTSTORE="${TLS_DOMAIN}.jks" \
- -p SSO_TRUSTSTORE_PASSWORD="changeme" \
- -p SSO_TRUSTSTORE_SECRET="sso-app-secret" \
- -p MEMORY_LIMIT="2Gi"
+ -p MEMORY_LIMIT="2Gi" \
+ -p IMAGE_URL='quay.io/justindav1s/custom-sso:latest'
