a29d2b5ec535810ab51e5fb16b393e10a72f0e1d
Justin Davis <justinndavis@gmail.com>
2021-10-12 18:05:05 +0100

    updates

--- deploy74_with_secret/deploy.sh
+++ deploy74_with_secret/deploy.sh
@@ -2,24 +2,33 @@
 
 # login
 
-PROJECT=redhat-sso
+PROJECT=redhat-sso2
 
-oc project ${PROJECT}
+# oc delete project $PROJECT
+# oc new-project $PROJECT 2> /dev/null
+# while [ $? \> 0 ]; do
+#     sleep 1
+#     printf "."
+# oc new-project $PROJECT 2> /dev/null
+# done
 
+oc project $PROJECT
 oc delete deployment sso
 oc delete route -l application=sso
 oc delete service -l application=sso
-oc delete secret db-secret db.properties db.properies
+oc delete secret db-secret
+
+# jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle12c.oracle-test.svc.cluster.local})(PORT=1521))(CONNECT_DATA=(SERVER=dedicated)(SERVICE_NAME=ORCLCDB})))
+# jdbc:oracle:thin:@oracle12c.oracle-test.svc.cluster.local:1521:ORCLCDB
+# jdbc:oracle:thin:@(description=(address_list=(address=(protocol=tcp)(port=1521)(host=oracle12c.oracle-test.svc.cluster.local)))(connect_data=(INSTANCE_NAME=ORCLCDB)))
 
 oc policy add-role-to-user view system:serviceaccount:${PROJECT}:default
 
 oc create secret generic db-secret \
   --from-literal=DB_USER=keycloak \
   --from-literal=DB_PASSWORD=changeme \
-  --from-literal=DB_URL=jdbc:oracle:thin:@oracle12c.oracle-test.svc.cluster.local:1521:ORCLCDB
+  --from-literal=DB_URL="jdbc:oracle:thin:@(description=(address_list=(address=(protocol=tcp)(port=1521)(host=oracle12c.oracle-test.svc.cluster.local)))(connect_data=(SID=ORCLCDB)))"
 
-# oc create secret generic db-properties \
-#   --from-file=secrets/db.properties  
 
 oc new-app -f sso74-https.yaml \
  -p APPLICATION_NAME="sso" \
