b7a65d27070bdb75156369382359ee8582722a93
Justin Davis <justinndavis@gmail.com>
2021-10-12 09:38:51 +0100

    deployment updates

--- deploy74/deploy.sh
+++ deploy74/deploy.sh
@@ -4,13 +4,13 @@ oc login https://${IP} -u justin
 
 PROJECT=redhat-sso
 
-# oc delete project $PROJECT
-# oc new-project $PROJECT 2> /dev/null
-# while [ $? \> 0 ]; do
-#     sleep 1
-#     printf "."
-# oc new-project $PROJECT 2> /dev/null
-# done
+oc delete project $PROJECT
+oc new-project $PROJECT 2> /dev/null
+while [ $? \> 0 ]; do
+    sleep 1
+    printf "."
+oc new-project $PROJECT 2> /dev/null
+done
 
 
 oc project $PROJECT
@@ -20,4 +20,5 @@ oc policy add-role-to-user view system:serviceaccount:${PROJECT}:default
 oc new-app -f sso74-https.yaml \
  -p APPLICATION_NAME="sso" \
  -p SSO_REALM="demorealm" \
- -p MEMORY_LIMIT="2Gi"
+ -p MEMORY_LIMIT="2Gi" \
+ -p IMAGE_URL='quay.io/justindav1s/custom-sso:latest'
