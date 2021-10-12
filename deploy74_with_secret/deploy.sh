2c34272dea8896b6309996f8d5c99fa89b607ad8
Justin Davis <justinndavis@gmail.com>
2021-10-12 15:40:54 +0100

    updates

--- deploy74_with_secret/deploy.sh
+++ deploy74_with_secret/deploy.sh
@@ -18,6 +18,9 @@ oc create secret generic db-secret \
   --from-literal=DB_PASSWORD=changeme \
   --from-literal=DB_URL=jdbc:oracle:thin:@oracle12c.oracle-test.svc.cluster.local:1521:ORCLCDB
 
+oc create secret generic db-properies \
+  --from-file=secrets/db.properies  
+
 oc new-app -f sso74-https.yaml \
  -p APPLICATION_NAME="sso" \
  -p SSO_REALM="demorealm" \
